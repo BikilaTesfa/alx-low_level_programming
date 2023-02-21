ba8e8fb7f6017cbfb025df268826b46da3046c08
BikilaTesfa <bikogif@gmail.com>
2023-02-21 12:45:46 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - the main function of the code
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char first[] = "_putchar";
+int i;
+for (i =  0; i < 8; i++)
+{
+_putchar(first[i]);
+}
+_putchar('\n');
+return (0);
+}
