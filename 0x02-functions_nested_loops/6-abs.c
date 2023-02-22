ab66144b7338d6090853f234ac7a9dfe94b3b98d
BikilaTesfa <bikogif@gmail.com>
2023-02-22 09:28:40 -0800

    all in one

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - is used to return absolute value values
+ * Return: positive value for both positive; and 0 for zero
+ *@i: integer to calculate abs
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+i = (-1 * i);
+return (i);
+}
+else if (i > 0)
+{
+return (i);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - this code print the last digit of an integer
+ * Return: 0 success
+ * @i: integer to print the last digit of it
+ */
+int print_last_digit(int i)
+{
+int ld = i % 10;
+if (i < 0)
+{
+ld *= -1;
+}
+_putchar(ld + '0');
+return (ld);
+}
