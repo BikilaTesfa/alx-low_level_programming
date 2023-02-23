80f3a2e4c16c002ac9199ba0806fc238115dcdc5
BikilaTesfa <bikogif@gmail.com>
2023-02-23 10:23:59 -0800

    all in one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if the lettter is upper or lowercase
+ * @c: print integer
+ * Return: returns 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
