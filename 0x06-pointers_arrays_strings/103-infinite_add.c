d4849456932e8274e92551945c759bdb83458b1b
BikilaTesfa <bikogif@gmail.com>
2023-03-02 13:11:14 -0800

    strcmp

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,7 +23,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if ((biggest + 1) >= size_r)
 		return (0);
 	r[biggest + 1] = '\0';
-
 	while (biggest >= 0)
 	{
 		a = (n1[a_len - 1] - '0');
@@ -36,7 +35,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum = a + carry;
 		else
 			sum = carry;
-
 		if (sum > 9)
 		{
 			carry = sum / 10;
